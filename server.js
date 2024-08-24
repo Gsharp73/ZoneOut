b1004bd239dda4e11034393de96c7699e9566da7
Dhyanendraa2004 <tripathidhyanendra2004@gmail.com>
2024-08-24 22:13:02 +0530

    update1

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
